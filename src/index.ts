5387b614e80196ba512a2329090e0d4b20097185
Angelin Sneha V <angelin.sneha@blueoptima.com>
2024-04-26 12:39:18 +0530

    Revert "add package"
    This reverts commit 6528c33305178686e5eaaa65669522cdcaa2172e.

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,10 @@ import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
-import { ExpressAdapter } from '@bull-board/express';
 
 const app: Express = express();
 
 app.use('/api', apiRouter);
-const serverAdapter = new ExpressAdapter();
-
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at *:${serverConfig.PORT}`);
