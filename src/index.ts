3a6653f4555e8e32a589ab839477c8e90f8e51a7
Angelin Sneha V <angelin.sneha@blueoptima.com>
2024-05-04 13:56:24 +0530

    integrate bullboard ui

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import logger from "./config/loggerConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
@@ -9,10 +10,12 @@ import SampleWorker from "./workers/SampleWorker";
 const app: Express = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server started at *:${serverConfig.PORT}`);
     logger.info(`LOGGER: Server started at *:${serverConfig.PORT}`);
+    console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
 
     // consumer/worker calling the job
     SampleWorker('SampleQueue');
